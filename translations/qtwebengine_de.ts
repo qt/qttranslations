85e12b885e93a9f086b6a29f3cfb4a9a632013f4
Friedemann Kleint <Friedemann.Kleint@qt.io>
2019-03-05 09:37:18 +0100

    Add missing German translations of QtWebEngine
    Task-number: QTBUG-74152
    Change-Id: I4ee991557d5af25fef782fc7838c5bb58c77df89
    Reviewed-by: Robert Loehning <robert.loehning@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@qt.io>

--- translations/qtwebengine_de.ts
+++ translations/qtwebengine_de.ts
@@ -2,6 +2,105 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
+    <name>DownloadInterruptReason</name>
+    <message>
+        <source>Unknown reason or not interrupted</source>
+        <translation>Unbekannter Grund oder nicht unterbrochen</translation>
+    </message>
+    <message>
+        <source>General file operation failure</source>
+        <translation>Allgemeiner Fehler bei Dateioperation</translation>
+    </message>
+    <message>
+        <source>The file cannot be written locally, due to access restrictions</source>
+        <translation>Die Datei kann auf Grund fehlender Zugriffsrechte nicht lokal gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Insufficient space on the target drive</source>
+        <translation>Unzureichender Platz auf dem Ziellaufwerk</translation>
+    </message>
+    <message>
+        <source>The directory or file name is too long</source>
+        <translation>Der Name des Verzeichnisses beziehungsweise der Datei ist zu lang</translation>
+    </message>
+    <message>
+        <source>The file size exceeds the file system limitation</source>
+        <translation>Die Dateigröße überschreitet das Limit des Dateisystems</translation>
+    </message>
+    <message>
+        <source>The file is infected with a virus</source>
+        <translation>Die Datei ist mit einem Virus infiziert</translation>
+    </message>
+    <message>
+        <source>Temporary problem (for example file in use, or too many open files)</source>
+        <translation>Temporäres Problem (zum Beispiel kann die Datei gerade in Benutzung sein oder es sind zu viele Dateien geöffnet)</translation>
+    </message>
+    <message>
+        <source>The file was blocked due to local policy</source>
+        <translation>Die Datei ist auf Grund einer lokalen Richtlinie gesperrt</translation>
+    </message>
+    <message>
+        <source>Checking the safety of the download failed due to unexpected reasons</source>
+        <translation>Die Überprüfung der Sicherheit des Downloads schlug aus unerwarteten Gründen fehl</translation>
+    </message>
+    <message>
+        <source>File seek past the end of a file (resuming previously interrupted download)</source>
+        <translation>Eine Dateisuchoperation ging über das Dateiende hinaus (bei Fortsetzung eines unterbrochenen Downloads)</translation>
+    </message>
+    <message>
+        <source>The partial file did not match the expected hash</source>
+        <translation>Die angefangene Datei entspricht nicht dem gespeicherten Hash</translation>
+    </message>
+    <message>
+        <source>General network failure</source>
+        <translation>Allgemeiner Netzwerkfehler</translation>
+    </message>
+    <message>
+        <source>The network operation has timed out</source>
+        <translation>Zeitüberschreitung bei der Netzwerkoperation</translation>
+    </message>
+    <message>
+        <source>The network connection has been terminated</source>
+        <translation>Die Netzwerkverbindung wurde unterbrochen</translation>
+    </message>
+    <message>
+        <source>The server has gone down</source>
+        <translation>Der Server wurde heruntergefahren</translation>
+    </message>
+    <message>
+        <source>The network request was invalid (for example, the URL or scheme is invalid)</source>
+        <translation>Die Netzwerkanforderung war ungültig (zum Beispiel auf Grund eines ungültigen Links oder Schemas)</translation>
+    </message>
+    <message>
+        <source>General server failure</source>
+        <translation>Allgemeiner Serverfehler</translation>
+    </message>
+    <message>
+        <source>The server does not have the requested data</source>
+        <translation>Der Server verfügt nicht über die angeforderten Daten</translation>
+    </message>
+    <message>
+        <source>The server did not authorize access to the resource</source>
+        <translation>Der Server hat den Zugriff auf die Ressource nicht autorisiert</translation>
+    </message>
+    <message>
+        <source>A problem with the server certificate occurred</source>
+        <translation>Es trat ein Problem mit dem Zertifikat des Servers auf</translation>
+    </message>
+    <message>
+        <source>Access forbidden by the server</source>
+        <translation>Der Server gestattet den Zugriff nicht</translation>
+    </message>
+    <message>
+        <source>Unexpected server response</source>
+        <translation>Unerwartete Antwort des Servers</translation>
+    </message>
+    <message>
+        <source>Download canceled by the user</source>
+        <translation>Der Download wurde vom Nutzer abgebrochen</translation>
+    </message>
+</context>
+<context>
     <name>QQuickWebEngineView</name>
     <message>
         <source>Stop</source>
@@ -245,6 +344,109 @@
     </message>
 </context>
 <context>
+    <name>RenderViewContextMenuQt</name>
+    <message>
+        <source>Back</source>
+        <translation>Zurück</translation>
+    </message>
+    <message>
+        <source>Forward</source>
+        <translation>Vorwärts</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Neu laden</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Ausschneiden</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopieren</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Einfügen</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Rückgängig</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Wiederholen</translation>
+    </message>
+    <message>
+        <source>Select all</source>
+        <translation>Alles auswählen</translation>
+    </message>
+    <message>
+        <source>Paste and match style</source>
+        <translation>Einfügen und Stil anpassen</translation>
+    </message>
+    <message>
+        <source>Open link in new window</source>
+        <translation>Link in neuem Fenster öffnen</translation>
+    </message>
+    <message>
+        <source>Open link in new tab</source>
+        <translation>Link in neuem Reiter öffnen</translation>
+    </message>
+    <message>
+        <source>Copy link address</source>
+        <translation>Adresse des Links kopieren</translation>
+    </message>
+    <message>
+        <source>Save link</source>
+        <translation>Link speichern</translation>
+    </message>
+    <message>
+        <source>Copy image</source>
+        <translation>Bild kopieren</translation>
+    </message>
+    <message>
+        <source>Copy image address</source>
+        <translation>Adresse des Bildes kopieren</translation>
+    </message>
+    <message>
+        <source>Save image</source>
+        <translation>Bild speichern</translation>
+    </message>
+    <message>
+        <source>Copy media address</source>
+        <translation>Medienadresse kopieren</translation>
+    </message>
+    <message>
+        <source>Show controls</source>
+        <translation>Steuerelemente anzeigen</translation>
+    </message>
+    <message>
+        <source>Loop</source>
+        <translation>Dauerschleife</translation>
+    </message>
+    <message>
+        <source>Save media</source>
+        <translation>Medien speichern</translation>
+    </message>
+    <message>
+        <source>Inspect</source>
+        <translation>Inspizieren</translation>
+    </message>
+    <message>
+        <source>Exit full screen</source>
+        <translation>Vollbildmodus beenden</translation>
+    </message>
+    <message>
+        <source>Save page</source>
+        <translation>Seite speichern</translation>
+    </message>
+    <message>
+        <source>View page source</source>
+        <translation>Quelltext der Seite anzeigen</translation>
+    </message>
+</context>
+<context>
     <name>UIDelegatesManager</name>
     <message>
         <source>Connect to proxy &quot;%1&quot; using:</source>
@@ -253,6 +455,13 @@
     <message>
         <source>Enter username and password for &quot;%1&quot; at %2://%3</source>
         <translation>Geben Sie Nutzername und Passwort für &quot;%1&quot; auf %2://%3 ein</translation>
+    </message>
+</context>
+<context>
+    <name>WebContentsAdapter</name>
+    <message>
+        <source>HTTP-POST data can only be sent over HTTP(S) protocol</source>
+        <translation>HTTP-POST-Daten können nur über das HTTP(S)-Protokoll versendet werden</translation>
     </message>
 </context>
 </TS>
