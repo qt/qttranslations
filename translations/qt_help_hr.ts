a6005539934e771bcf22bed6b66d16bdbf5e6283
milotype <43657314+milotype@users.noreply.github.com>
2019-05-01 00:26:28 +0200

    Croatian translation 100% qt_help_hr.ts

---
+++ translations/qt_help_hr.ts
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr">
+<context>
+    <name>QHelp</name>
+    <message>
+        <location filename="../../qttools/src/assistant/help/qhelp_global.cpp" line="+60"/>
+        <source>Untitled</source>
+        <translation>Bezimeno</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpCollectionHandler</name>
+    <message>
+        <location filename="../../qttools/src/assistant/help/qhelpcollectionhandler.cpp" line="+108"/>
+        <source>The collection file &quot;%1&quot; is not set up yet.</source>
+        <translation>Zbirna datoteka „%1” još nije postavljena.</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Cannot load sqlite database driver.</source>
+        <translation>Nije moguće učitati upravljački program sqlite baze podataka.</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <location line="+220"/>
+        <source>Cannot open collection file: %1</source>
+        <translation>Nije moguće otvoriti zbirnu datoteku: %1</translation>
+    </message>
+    <message>
+        <location line="-201"/>
+        <source>Cannot create tables in file %1.</source>
+        <translation>Nije moguće izraditi tablice u datoteci „%1”.</translation>
+    </message>
+    <message>
+        <location line="+36"/>
+        <source>Cannot create index tables in file %1.</source>
+        <translation>Nije moguće izraditi tablice indeksa u datoteci „%1”.</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Cannot register index tables in file %1.</source>
+        <translation>Nije moguće prijaviti tablice indeksa u datoteci „%1”.</translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>Cannot unregister index tables in file %1.</source>
+        <translation>Nije moguće odjaviti tablice indeksa u datoteci „%1”.</translation>
+    </message>
+    <message>
+        <location line="+98"/>
+        <source>The collection file &quot;%1&quot; already exists.</source>
+        <translation>Zbirna datoteka „%1” već postoji.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Cannot create directory: %1</source>
+        <translation>Nije moguće izraditi mapu: %1</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Cannot copy collection file: %1</source>
+        <translation>Nije moguće kopirati zbirnu datoteku: %1</translation>
+    </message>
+    <message>
+        <location line="+383"/>
+        <source>Unknown filter &quot;%1&quot;.</source>
+        <translation>Nepoznati filtar „%1”.</translation>
+    </message>
+    <message>
+        <location line="+54"/>
+        <source>Cannot register filter %1.</source>
+        <translation>Nije moguće prijaviti filtar „%1”.</translation>
+    </message>
+    <message>
+        <location line="+82"/>
+        <source>Cannot open documentation file %1.</source>
+        <translation>Nije moguće otvoriti datoteku dokumentacije %1.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Invalid documentation file &quot;%1&quot;.</source>
+        <translation>Nevaljana datoteka dokumentacije „%1”.</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <location line="+16"/>
+        <source>The namespace %1 was not registered.</source>
+        <translation>Nazivni prostor „%1” nije prijavljen.</translation>
+    </message>
+    <message>
+        <location line="+884"/>
+        <source>Namespace %1 already exists.</source>
+        <translation>Nazivni prostor „%1” već postoji.</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Cannot register namespace &quot;%1&quot;.</source>
+        <translation>Nije moguće prijaviti nazivni prostor „%1”.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Cannot register virtual folder &apos;%1&apos;.</source>
+        <translation>Nije moguće prijaviti virtualnu mapu „%1”.</translation>
+    </message>
+    <message>
+        <location line="+106"/>
+        <source>Version %1</source>
+        <translation>Verzija %1</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpDBReader</name>
+    <message>
+        <location filename="../../qttools/src/assistant/help/qhelpdbreader.cpp" line="+103"/>
+        <source>Cannot open database &quot;%1&quot; &quot;%2&quot;: %3</source>
+        <extracomment>The placeholders are: %1 - The name of the database which cannot be opened %2 - The unique id for the connection %3 - The actual error string</extracomment>
+        <translation>Nije moguće otvoriti bazu podataka „%1” „%2”: %3</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpSearchQueryWidget</name>
+    <message>
+        <location filename="../../qttools/src/assistant/help/qhelpsearchquerywidget.cpp" line="+113"/>
+        <source>Search for:</source>
+        <translation>Traži:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Previous search</source>
+        <translation>Prethodna pretraga</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Next search</source>
+        <translation>Sljedeća pretraga</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Search</source>
+        <translation>Traži</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpSearchResultWidget</name>
+    <message numerus="yes">
+        <location filename="../../qttools/src/assistant/help/qhelpsearchresultwidget.cpp" line="+211"/>
+        <source>%1 - %2 of %n Hits</source>
+        <translation>
+            <numerusform>%1 - %2 od %n pogotka</numerusform>
+            <numerusform>%1 - %2 od %n pogotka</numerusform>
+            <numerusform>%1 - %2 od %n pogodaka</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location line="+59"/>
+        <source>0 - 0 of 0 Hits</source>
+        <translation>0 - 0 od 0 pogodaka</translation>
+    </message>
+</context>
+<context>
+    <name>QResultWidget</name>
+    <message>
+        <location line="-195"/>
+        <source>Search Results</source>
+        <translation>Rezultati pretrage</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Note:</source>
+        <translation>Napomena:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>The search results may not be complete since the documentation is still being indexed.</source>
+        <translation>Rezultati pretrage možda neće biti potpuni, jer se dokumentacija još uvijek indeksira.</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Your search did not match any documents.</source>
+        <translation>Nijedan dokument se ne poklapa s tvojom pretragom.</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>(The reason for this might be that the documentation is still being indexed.)</source>
+        <translation>(Razlog tome može biti, da se dokumentacija još uvijek indeksira.)</translation>
+    </message>
+</context>
+<context>
+    <name>fulltextsearch::qt::QHelpSearchIndexWriter</name>
+    <message>
+        <location filename="../../qttools/src/assistant/help/qhelpsearchindexwriter_default.cpp" line="+78"/>
+        <source>Cannot open database &quot;%1&quot; using connection &quot;%2&quot;: %3</source>
+        <translation>Nije moguće otvoriti bazu podataka „%1” koja koristi vezu „%2”: %3</translation>
+    </message>
+</context>
+</TS>
