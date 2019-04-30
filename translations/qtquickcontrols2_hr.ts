4a30386fb1b7d527da9081ae1afc9863985d8c7f
milotype <43657314+milotype@users.noreply.github.com>
2019-05-01 00:29:09 +0200

    Croatian translation 100% qtquickcontrols2_hr.ts

---
+++ translations/qtquickcontrols2_hr.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr">
+<context>
+    <name>QQuickPlatformDialog</name>
+    <message>
+        <location filename="../../qtquickcontrols2/src/imports/platform/qtlabsplatformplugin.cpp" line="+83"/>
+        <source>Dialog is an abstract base class</source>
+        <translation>Dijalog je apstraktna osnovna klasa</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Cannot create an instance of StandardButton</source>
+        <translation>Nije moguće izraditi standardni gumb</translation>
+    </message>
+</context>
+<context>
+    <name>QtQuickControls2ImagineStylePlugin</name>
+    <message>
+        <location filename="../../qtquickcontrols2/src/imports/controls/imagine/qtquickcontrols2imaginestyleplugin.cpp" line="+71"/>
+        <source>Imagine is an attached property</source>
+        <translation>Imagine je priloženo svojstvo</translation>
+    </message>
+</context>
+<context>
+    <name>QtQuickControls2MaterialStylePlugin</name>
+    <message>
+        <location filename="../../qtquickcontrols2/src/imports/controls/material/qtquickcontrols2materialstyleplugin.cpp" line="+72"/>
+        <source>Material is an attached property</source>
+        <translation>Material je priloženo svojstvo</translation>
+    </message>
+</context>
+<context>
+    <name>QtQuickControls2UniversalStylePlugin</name>
+    <message>
+        <location filename="../../qtquickcontrols2/src/imports/controls/universal/qtquickcontrols2universalstyleplugin.cpp" line="+69"/>
+        <source>Universal is an attached property</source>
+        <translation>Universal je priloženo svojstvo</translation>
+    </message>
+</context>
+</TS>
