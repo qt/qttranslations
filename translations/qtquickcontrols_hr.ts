aeb2e2a29d27397a0ef7afffcaa59133f19d069a
milotype <43657314+milotype@users.noreply.github.com>
2019-05-01 00:28:38 +0200

    Croatian translation 100% qtquickcontrols_hr.ts

---
+++ translations/qtquickcontrols_hr.ts
@@ -0,0 +1,373 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr">
+<context>
+    <name>DefaultColorDialog</name>
+    <message>
+        <location filename="../../qtquickcontrols/src/dialogs/DefaultColorDialog.qml" line="+281"/>
+        <source>Hue</source>
+        <translation>Nijansa</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Saturation</source>
+        <translation>Zasićenost</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Luminosity</source>
+        <translation>Osvijetljenost</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Alpha</source>
+        <translation>Alfa</translation>
+    </message>
+    <message>
+        <location line="+59"/>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>OK</source>
+        <translation>U redu</translation>
+    </message>
+</context>
+<context>
+    <name>DefaultDialogWrapper</name>
+    <message>
+        <location filename="../../qtquickcontrols/src/dialogs/DefaultDialogWrapper.qml" line="+109"/>
+        <source>Show Details...</source>
+        <translation>Prikaži detalje …</translation>
+    </message>
+</context>
+<context>
+    <name>DefaultFileDialog</name>
+    <message>
+        <location filename="../../qtquickcontrols/src/dialogs/DefaultFileDialog.qml" line="+147"/>
+        <source>Go up to the folder containing this one</source>
+        <translation>Prijeđi u mapu, koja ovo sadrži</translation>
+    </message>
+    <message>
+        <location line="+121"/>
+        <source>Remove favorite</source>
+        <translation>Ukloni favorit</translation>
+    </message>
+    <message>
+        <location line="+42"/>
+        <source>Add the current directory as a favorite</source>
+        <translation>Dodaj trenutačnu mapu u favorite</translation>
+    </message>
+    <message>
+        <location line="+38"/>
+        <source>Filename</source>
+        <translation>Ime datoteke</translation>
+    </message>
+    <message>
+        <location line="+27"/>
+        <source>Type</source>
+        <comment>file type (extension)</comment>
+        <translation>Vrsta</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Size</source>
+        <comment>file size</comment>
+        <translation>Veličina</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Modified</source>
+        <comment>last-modified time</comment>
+        <translation>Posljednja promjena</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Accessed</source>
+        <comment>last-accessed time</comment>
+        <translation>Posljednja upotreba</translation>
+    </message>
+    <message>
+        <location line="+65"/>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Choose</source>
+        <translation>Odaberi</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Open</source>
+        <translation>Otvori</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Save</source>
+        <translation>Spremi</translation>
+    </message>
+</context>
+<context>
+    <name>DefaultFontDialog</name>
+    <message>
+        <location filename="../../qtquickcontrols/src/dialogs/DefaultFontDialog.qml" line="+120"/>
+        <source>Font</source>
+        <translation>Font</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <location line="+81"/>
+        <source>Weight</source>
+        <translation>Rez</translation>
+    </message>
+    <message>
+        <location line="-80"/>
+        <location line="+154"/>
+        <source>Size</source>
+        <translation>Veličina</translation>
+    </message>
+    <message>
+        <location line="-141"/>
+        <source>Font Family</source>
+        <translation>Obitelj fontova</translation>
+    </message>
+    <message>
+        <location line="+70"/>
+        <source>Thin</source>
+        <translation>Tanki</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>ExtraLight</source>
+        <translation>Ekstra svijetli</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Light</source>
+        <translation>Svijetli</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Normal</source>
+        <translation>Normalni</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Medium</source>
+        <translation>Srednji</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>DemiBold</source>
+        <translation>Poludebeli</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Bold</source>
+        <translation>Debeli</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>ExtraBold</source>
+        <translation>Ekstra debeli</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Black</source>
+        <translation>Masni</translation>
+    </message>
+    <message>
+        <location line="+90"/>
+        <source>Style</source>
+        <translation>Stil</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Italic</source>
+        <translation>Kurziv</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Underline</source>
+        <translation>Podcrtano</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Overline</source>
+        <translation>Nadcrtano</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Strikeout</source>
+        <translation>Precrtano</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Writing System</source>
+        <translation>Pismovni sustav</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Sample</source>
+        <translation>Primjer</translation>
+    </message>
+    <message>
+        <location line="+52"/>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>OK</source>
+        <translation>U redu</translation>
+    </message>
+</context>
+<context>
+    <name>DefaultMessageDialog</name>
+    <message>
+        <location filename="../../qtquickcontrols/src/dialogs/DefaultMessageDialog.qml" line="+142"/>
+        <source>OK</source>
+        <translation>U redu</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Open</source>
+        <translation>Otvori</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Save</source>
+        <translation>Spremi</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Save All</source>
+        <translation>Spremi sve</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Retry</source>
+        <translation>Pokušaj ponovo</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Ignore</source>
+        <translation>Zanemari</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Apply</source>
+        <translation>Primijeni</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Yes</source>
+        <translation>Da</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Yes to All</source>
+        <translation>Da, za sve</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>No</source>
+        <translation>Ne</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>No to All</source>
+        <translation>Ne, za sve</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Discard</source>
+        <translation>Odbaci</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Reset</source>
+        <translation>Resetiraj</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Restore Defaults</source>
+        <translation>Vrati na zadane vrijednosti</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Abort</source>
+        <translation>Prekini</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Close</source>
+        <translation>Zatvori</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Show Details...</source>
+        <translation>Prikaži detalje …</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Help</source>
+        <translation>Pomoć</translation>
+    </message>
+    <message>
+        <location line="+56"/>
+        <source>Hide Details</source>
+        <translation>Sakrij detalje</translation>
+    </message>
+</context>
+<context>
+    <name>EditMenu_base</name>
+    <message>
+        <location filename="../../qtquickcontrols/src/controls/Private/EditMenu_base.qml" line="+51"/>
+        <source>&amp;Undo</source>
+        <translation>&amp;Poništi</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>&amp;Redo</source>
+        <translation>Po&amp;novi</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Cu&amp;t</source>
+        <translation>&amp;Izreži</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopiraj</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>&amp;Paste</source>
+        <translation>&amp;Zalijepi</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Delete</source>
+        <translation>Ukloni</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Clear</source>
+        <translation>Izbriši</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Select All</source>
+        <translation>Odaberi sve</translation>
+    </message>
+</context>
+</TS>
