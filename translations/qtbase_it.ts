88d6e2819db4943ff0e01e0957daf94de72f3d0f
Montelion <49426949+Montelion@users.noreply.github.com>
2019-12-23 19:27:26 +0100

    Updated Italian translation

--- translations/qtbase_it.ts
+++ translations/qtbase_it.ts
@@ -689,7 +689,7 @@ Premi ESC per annullare</translation>
     </message>
     <message>
         <source>Discard</source>
-        <translation type="vanished">Tralascia</translation>
+        <translation type="vanished">Non salvare</translation>
     </message>
     <message>
         <source>&amp;Yes</source>
@@ -3518,7 +3518,7 @@ Vuoi eliminarlo comunque?</translation>
     </message>
     <message>
         <source>Discard</source>
-        <translation>Tralascia</translation>
+        <translation>Non salvare</translation>
     </message>
     <message>
         <source>Help</source>
