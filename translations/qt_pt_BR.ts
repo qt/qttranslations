640294614d38280c99c152ab7b7625f561325ec7
Friedemann Kleint <Friedemann.Kleint@qt.io>
2021-06-14 09:12:30 +0200

    Remove obsolete modules from catalogs
    Task-number: PYSIDE-1598
    Change-Id: I23f67fa6fc9559eed143c51038c95a1dbec13a1c
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@gmx.de>

--- translations/qt_pt_BR.ts
+++ translations/qt_pt_BR.ts
@@ -3,8 +3,6 @@
 <TS version="2.1" language="pt_BR">
 <dependencies>
 <dependency catalog="qtbase_"/>
-<dependency catalog="qtscript_"/>
 <dependency catalog="qtmultimedia_"/>
-<dependency catalog="qtxmlpatterns_"/>
 </dependencies>
 </TS>
