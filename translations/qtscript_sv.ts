ea21a361601ef5d40fa876bc5c085e4344fa5541
Daniel Nylander <qt@danielnylander.se>
2025-02-16 20:47:17 +0100

    Add Swedish translation
    Fixes: QTBUG-132800
    Change-Id: I076cf967d962477588cdfc8889558f5f51c27133
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@gmx.de>

---
+++ translations/qtscript_sv.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
+<context>
+    <name>QScriptBreakpointsModel</name>
+    <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Plats</translation>
+    </message>
+    <message>
+        <source>Condition</source>
+        <translation>Villkor</translation>
+    </message>
+    <message>
+        <source>Ignore-count</source>
+        <translation>Ignorera antal</translation>
+    </message>
+    <message>
+        <source>Single-shot</source>
+        <translation>Enstaka skott</translation>
+    </message>
+    <message>
+        <source>Hit-count</source>
+        <translation>Antal träffar</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptBreakpointsWidget</name>
+    <message>
+        <source>New</source>
+        <translation>Ny</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebugger</name>
+    <message>
+        <source>Go to Line</source>
+        <translation>Gå till rad</translation>
+    </message>
+    <message>
+        <source>Line:</source>
+        <translation>Rad:</translation>
+    </message>
+    <message>
+        <source>Interrupt</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Shift+F5</source>
+        <translation>Skift+F5</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Fortsätt</translation>
+    </message>
+    <message>
+        <source>F5</source>
+        <translation>F5</translation>
+    </message>
+    <message>
+        <source>Step Into</source>
+        <translation>Stega in i</translation>
+    </message>
+    <message>
+        <source>F11</source>
+        <translation>F11</translation>
+    </message>
+    <message>
+        <source>Step Over</source>
+        <translation>Stega över</translation>
+    </message>
+    <message>
+        <source>F10</source>
+        <translation>F10</translation>
+    </message>
+    <message>
+        <source>Step Out</source>
+        <translation>Stega ut</translation>
+    </message>
+    <message>
+        <source>Shift+F11</source>
+        <translation>Skift+F11</translation>
+    </message>
+    <message>
+        <source>Run to Cursor</source>
+        <translation>Kör till markör</translation>
+    </message>
+    <message>
+        <source>Ctrl+F10</source>
+        <translation>Ctrl+F10</translation>
+    </message>
+    <message>
+        <source>Run to New Script</source>
+        <translation>Kör till nytt skript</translation>
+    </message>
+    <message>
+        <source>Toggle Breakpoint</source>
+        <translation>Växla brytpunkt</translation>
+    </message>
+    <message>
+        <source>F9</source>
+        <translation>F9</translation>
+    </message>
+    <message>
+        <source>Clear Debug Output</source>
+        <translation>Töm felsökningsutdata</translation>
+    </message>
+    <message>
+        <source>Clear Error Log</source>
+        <translation>Töm fellogg</translation>
+    </message>
+    <message>
+        <source>Clear Console</source>
+        <translation>Töm konsoll</translation>
+    </message>
+    <message>
+        <source>&amp;Find in Script...</source>
+        <translation>&amp;Sök i skript...</translation>
+    </message>
+    <message>
+        <source>Ctrl+F</source>
+        <translation>Ctrl+F</translation>
+    </message>
+    <message>
+        <source>Find &amp;Next</source>
+        <translation>Hitta &amp;nästa</translation>
+    </message>
+    <message>
+        <source>F3</source>
+        <translation>F3</translation>
+    </message>
+    <message>
+        <source>Find &amp;Previous</source>
+        <translation>Hitta &amp;föregående</translation>
+    </message>
+    <message>
+        <source>Shift+F3</source>
+        <translation>Skift+F3</translation>
+    </message>
+    <message>
+        <source>Ctrl+G</source>
+        <translation>Ctrl+G</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation>Felsök</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebuggerCodeFinderWidget</name>
+    <message>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+    <message>
+        <source>Previous</source>
+        <translation>Föregående</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Nästa</translation>
+    </message>
+    <message>
+        <source>Case Sensitive</source>
+        <translation>Skiftlägeskänslig</translation>
+    </message>
+    <message>
+        <source>Whole words</source>
+        <translation>Hela ord</translation>
+    </message>
+    <message>
+        <source>&lt;img src=&quot;:/qt/scripttools/debugging/images/wrap.png&quot;&gt;&amp;nbsp;Search wrapped</source>
+        <translation>&lt;img src=&quot;:/qt/scripttools/debugging/images/wrap.png&quot;&gt;&amp;nbsp;Sökningen nådde slutet</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebuggerLocalsModel</name>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>Value</source>
+        <translation>Värde</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebuggerStackModel</name>
+    <message>
+        <source>Level</source>
+        <translation>Nivå</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Plats</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptEdit</name>
+    <message>
+        <source>Toggle Breakpoint</source>
+        <translation>Växla brytpunkt</translation>
+    </message>
+    <message>
+        <source>Disable Breakpoint</source>
+        <translation>Inaktivera brytpunkt</translation>
+    </message>
+    <message>
+        <source>Enable Breakpoint</source>
+        <translation>Aktivera brytpunkt</translation>
+    </message>
+    <message>
+        <source>Breakpoint Condition:</source>
+        <translation>Brytpunktsvillkor:</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptEngineDebugger</name>
+    <message>
+        <source>Loaded Scripts</source>
+        <translation>Inlästa skript</translation>
+    </message>
+    <message>
+        <source>Breakpoints</source>
+        <translation>Brytpunkter</translation>
+    </message>
+    <message>
+        <source>Stack</source>
+        <translation>Stack</translation>
+    </message>
+    <message>
+        <source>Locals</source>
+        <translation>Lokala</translation>
+    </message>
+    <message>
+        <source>Console</source>
+        <translation>Konsoll</translation>
+    </message>
+    <message>
+        <source>Debug Output</source>
+        <translation>Felsökningsutdata</translation>
+    </message>
+    <message>
+        <source>Error Log</source>
+        <translation>Fellogg</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation>Visa</translation>
+    </message>
+    <message>
+        <source>Qt Script Debugger</source>
+        <translation>Qt-skriptfelsökare</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptNewBreakpointWidget</name>
+    <message>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+</context>
+</TS>
