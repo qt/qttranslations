50471b9efec0d9c320c2de11b30f27309f124353
Baurzhan Muftakhidinov <baurthefirst@gmail.com>
2026-08-02 16:41:30 +0500

    Add Kazakh translations for multiple Qt modules
    Change-Id: I19b17dec0e050864d29c226fc9915b392ee1e6ad
    Signed-off-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Reviewed-by: Ildar Mustafin
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@gmx.de>

---
+++ translations/qt_kk.ts
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kk_KZ">
+<dependencies>
+<dependency catalog="qtbase_"/>
+<dependency catalog="qtmultimedia_"/>
+</dependencies>
+</TS>
